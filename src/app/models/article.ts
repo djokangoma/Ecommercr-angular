a616618d9a6f31060871f25068c709d8cd4ab1c1
djo kangoma <djokangoma@gmail.com>
2025-01-24 15:14:11 +0100

    refactor: add a component arcticle-list

---
+++ src/app/models/article.ts
@@ -0,0 +1,12 @@
+export interface Article {
+    id: number,
+    title: string,
+    description : string,
+    price : number,
+    category : string,
+    image : string,
+    rating : {
+        rate:number,
+        count : number
+    }
+}
