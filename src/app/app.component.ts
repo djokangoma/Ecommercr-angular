e4f9d028d200a6b9d4cfb0178d0792004da3479d
djo kangoma <djokangoma@gmail.com>
2025-01-24 16:28:57 +0100

    refactor: handling different products removed from on api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ArticleComponent } from "./article/article.component";
+import { ArticleListComponent } from "./article-list/article-list.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ArticleComponent],
+  imports: [RouterOutlet, ArticleListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input} from '@angular/core';
 import { Article } from '../models/article';
 
 @Component({
@@ -7,20 +7,7 @@ import { Article } from '../models/article';
   templateUrl: './article.component.html',
   styleUrl: './article.component.css'
 })
-export class ArticleComponent {
-  article!:Article
-  ngOnInit():void{
-    this.article = {
-      id : 1,
-      title : "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
-      description : "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
-      price : 120,
-      category :"men's clothing",
-      image : "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
-      rating : {
-        rate:3.5,
-        count : 34
-      }
-    }
-  }
+export class ArticleComponent{
+  @Input() article!:Article
 }
+
