ecb3a0c4febd539e16c9d8283da60bbdd9b24761
djo kangoma <djokangoma@gmail.com>
2025-01-27 10:29:23 +0100

    feat: add singer component

---
+++ src/app/singer/singer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-singer',
+  imports: [],
+  templateUrl: './singer.component.html',
+  styleUrl: './singer.component.css'
+})
+export class SingerComponent {
+
+}
