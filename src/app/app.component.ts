d195c505cad722879bb0334f10f5c4890780a58a
djo kangoma <djokangoma@gmail.com>
2025-01-27 11:16:00 +0100

    feat:add component about

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ArticleComponent } from "./article/article.component";
 import { ArticleListComponent } from "./article-list/article-list.component";
+import { HeaderComponent } from "./header/header.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ArticleListComponent],
+  imports: [RouterOutlet, ArticleListComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
