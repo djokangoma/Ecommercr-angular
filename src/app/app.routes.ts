3426589bc195282bdf89431db2fb63c4c1a05927
djo kangoma <djokangoma@gmail.com>
2025-01-27 15:30:17 +0100

    feat: add create component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { ArticleListComponent } from './article-list/article-list.component';
+import { SingerComponent } from './singer/singer.component';
+import { AboutComponent } from './about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+{
+    path:'',
+    component :ArticleListComponent
+},
+
+{
+    path:'articles/:id',
+    component:SingerComponent,
+    
+},
+{
+    path:'about',
+    component:AboutComponent,
+    
+}
+];
--- src/app/singer/singer.component.ts
+++ src/app/singer/singer.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Article } from '../models/article';
+import { ArticleService } from '../services/article.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-singer',
@@ -7,5 +10,14 @@ import { Component } from '@angular/core';
   styleUrl: './singer.component.css'
 })
 export class SingerComponent {
+  route: ActivatedRoute = inject(ActivatedRoute)
+  article!: Article | undefined
+  service: ArticleService = inject(ArticleService)
+  articleId = -1
+
+  ngOnInit(){
+    this.articleId = Number(this.route.snapshot.paramMap.get("id"))
+    this.article = this.service.getOne(this.articleId)
+  }
 
 }
